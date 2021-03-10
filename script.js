1ca52ae9293eafe9e2df3b7fc989df1a62c92dca
bcurl21 <74270428+bcurl21@users.noreply.github.com>
2021-03-09 19:19:13 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,55 +1,109 @@
-    require([
-      "esri/Map",
-      "esri/layers/CSVLayer",
-      "esri/views/MapView",
-      "esri/config",
-      "esri/core/urlUtils",
-      "dojo/domReady!"
-    ], function(
-      Map,
-      CSVLayer,
-      MapView,
-      esriConfig,
-      urlUtils
-    ) {
+require(["esri/config",
+               "esri/Map", "esri/views/MapView", "esri/layers/FeatureLayer", "esri/widgets/Legend"], function(
+        esriConfig,
+         Map,
+        MapView,
+        FeatureLayer,
+        Legend
+      ) {
+        esriConfig.apiKey = "AAPKadbb47a8f4024f8694017868bb5f8514m-BINeDp-vV1s7yL8zwJeUAP-PbhOik8FZfNUXBo_uzR_cpQj_uun6RO5a5YScF7";
+        /******************************************************************
+         *
+         * Define symbols for each unique type. One each for freeways,
+         * highways, and other major roads
+         *
+         ******************************************************************/
 
-     
-     var url = "https://raw.githubusercontent.com/gbrunner/Advanced_Python_for_GIS_and_RS/master/Week%202/stl_crime_wgs_84.csv";
-     esriConfig.request.corsEnabledServers.push('https://rawgit.com');
-
-     
-
-        const template = {
-          title: "Earthquake Info",
-          content: "Magnitude {mag} {type} hit {place} on {time}."
+        // Symbol for freeways
+        const acquisitionSym = {
+          type: "simple-line", // autocasts as new SimpleLineSymbol()
+          color: "#74ff29",
+          width: "7px",
+          style: "solid"
         };
 
-        const csvLayer = new CSVLayer({
-          url: url,
-          copyright: "USGS Earthquakes",
-          popupTemplate: template
+        // Symbol for U.S. Highways
+        const TFASym = {
+          type: "simple-line", // autocasts as new SimpleLineSymbol()
+          color: "#f00f04",
+          width: "2px",
+          style: "solid"
+        };
+
+    
+
+        const transRenderer = {
+          type: "unique-value", // autocasts as new UniqueValueRenderer()
+          legendOptions: {
+            title: "Transaction"},          
+          defaultLabel: "I Don't Know",
+          field: "Transaction_",
+          uniqueValueInfos: [
+            {
+              value: "Acquisition", // code for interstates/freeways
+              symbol: acquisitionSym,
+              label: "Acquisition"
+            },
+            {
+              value: "Trade Away/Free Agency", // code for U.S. highways
+              symbol: TFASym,
+              label: "Trade Away/Free Agency"
+            }
+          ]
+        };
+
+  var stadiumsRenderer = {
+    type: "simple",
+    legendOptions: {
+            title: "Stadium"},
+    symbol: {
+      type: "simple-marker",
+      size: 11,
+      color: "white",
+      outline: {
+        width: 0.5,
+        color: "black"
+      }
+    }
+  };
+  
+  
+        // Set the renderer on the layer
+        const TransactionsLayer = new FeatureLayer({
+          url: "https://services2.arcgis.com/bB9Y1bGKerz1PTl5/arcgis/rest/services/map_(1)/FeatureServer",
+          renderer: transRenderer,
+          title: "Transactions",
+          minScale: 0,
+          maxScale: 0
         });
 
-        var symbol = {
-          type: "simple-marker", 
-          color:"green"
-        };
+        const StadiumsLayer = new FeatureLayer({
+          url: "https://services2.arcgis.com/bB9Y1bGKerz1PTl5/arcgis/rest/services/map_(12)/FeatureServer",
+          renderer: stadiumsRenderer
+        });
 
-      csvLayer.renderer = {
-        type: "simple", // autocasts as new SimpleRenderer()
-        symbol: symbol
-      };
+        // Add the layer to the map
+        const map = new Map({
+          layers: [TransactionsLayer, StadiumsLayer],
+          basemap: "arcgis-dark-gray"
+        });
 
-      var map = new Map({
-        basemap: "gray",
-        layers: [csvLayer]
+        const view = new MapView({
+          container: "viewDiv",
+          map: map,
+          center: [-98, 38],
+          zoom: 4
+        });
+
+
+        const legend = new Legend({
+          view: view,
+          layerInfos: [
+            {
+              layer: TransactionsLayer
+            }
+          ]
+        });
+
+        view.ui.add(legend, "bottom-left");
       });
-
-      var view = new MapView({
-        container: "viewDiv",
-        center: [-90.2, 38.63],
-        zoom: 12,
-        map: map
-      });
-
-    });
